81689f915995d4b241c7830ae4d7115f3460b3da
Mahadbaig12 <161740315+Mahadbaig12@users.noreply.github.com>
2024-04-26 18:22:35 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ let mahad = "";
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            mahad = eval(mahad);
+            document.querySelector('input').value = mahad;
+        }
+        else if(e.target.innerHTML == 'c'){
+            mahad = "";
+            document.querySelector('input').value = mahad;
+        }
+       else {
         console.log(e.target)
         mahad = mahad + e.target.innerHTML;
 document.querySelector('input').value = mahad;
+}
     })
 })
